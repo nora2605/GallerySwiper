02ed68d7df8618edb9caf62fd85e1b897fbe0a6b
nora2605 <nora.ja2605@gmail.com>
2024-08-10 01:05:35 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace GallerySwiper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox = new PictureBox();
+            buttonOpenFolder = new Button();
+            listboxCategories = new ListBox();
+            textboxCategory = new TextBox();
+            textboxShortcut = new TextBox();
+            labelNewCategory = new Label();
+            labelShortcut = new Label();
+            buttonSubmit = new Button();
+            buttonRemove = new Button();
+            buttonProcess = new Button();
+            labelCurrentFolder = new Label();
+            buttonHelp = new Button();
+            galleryFolderDialog = new FolderBrowserDialog();
+            textboxLog = new RichTextBox();
+            outputFolderDialog = new FolderBrowserDialog();
+            checkboxShouldMove = new CheckBox();
+            checkboxSpecialCats = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox.BackColor = SystemColors.ButtonShadow;
+            pictureBox.Location = new Point(159, 12);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(679, 426);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            pictureBox.Click += ResetFocus;
+            // 
+            // buttonOpenFolder
+            // 
+            buttonOpenFolder.Location = new Point(12, 12);
+            buttonOpenFolder.Name = "buttonOpenFolder";
+            buttonOpenFolder.Size = new Size(141, 23);
+            buttonOpenFolder.TabIndex = 1;
+            buttonOpenFolder.Text = "Open Folder...";
+            buttonOpenFolder.UseVisualStyleBackColor = true;
+            buttonOpenFolder.Click += buttonOpenFolder_Click;
+            buttonOpenFolder.KeyDown += Any_KeyDown;
+            // 
+            // listboxCategories
+            // 
+            listboxCategories.FormattingEnabled = true;
+            listboxCategories.ItemHeight = 15;
+            listboxCategories.Items.AddRange(new object[] { "Delete\tRight", "Keep\tLeft" });
+            listboxCategories.Location = new Point(12, 80);
+            listboxCategories.Name = "listboxCategories";
+            listboxCategories.Size = new Size(141, 94);
+            listboxCategories.TabIndex = 2;
+            listboxCategories.SelectedIndexChanged += listboxCategories_SelectedIndexChanged;
+            listboxCategories.KeyDown += Any_KeyDown;
+            // 
+            // textboxCategory
+            // 
+            textboxCategory.Location = new Point(12, 195);
+            textboxCategory.Name = "textboxCategory";
+            textboxCategory.Size = new Size(141, 23);
+            textboxCategory.TabIndex = 3;
+            // 
+            // textboxShortcut
+            // 
+            textboxShortcut.Location = new Point(12, 239);
+            textboxShortcut.Name = "textboxShortcut";
+            textboxShortcut.ReadOnly = true;
+            textboxShortcut.Size = new Size(141, 23);
+            textboxShortcut.TabIndex = 4;
+            textboxShortcut.KeyDown += Shortcut_KeyDown;
+            // 
+            // labelNewCategory
+            // 
+            labelNewCategory.AutoSize = true;
+            labelNewCategory.Location = new Point(12, 177);
+            labelNewCategory.Name = "labelNewCategory";
+            labelNewCategory.Size = new Size(90, 15);
+            labelNewCategory.TabIndex = 5;
+            labelNewCategory.Text = "Category Name";
+            // 
+            // labelShortcut
+            // 
+            labelShortcut.AutoSize = true;
+            labelShortcut.Location = new Point(12, 221);
+            labelShortcut.Name = "labelShortcut";
+            labelShortcut.Size = new Size(52, 15);
+            labelShortcut.TabIndex = 6;
+            labelShortcut.Text = "Shortcut";
+            // 
+            // buttonSubmit
+            // 
+            buttonSubmit.Location = new Point(12, 268);
+            buttonSubmit.Name = "buttonSubmit";
+            buttonSubmit.Size = new Size(62, 23);
+            buttonSubmit.TabIndex = 5;
+            buttonSubmit.Text = "Add";
+            buttonSubmit.UseVisualStyleBackColor = true;
+            buttonSubmit.Click += buttonSubmit_Click;
+            buttonSubmit.KeyDown += Any_KeyDown;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.Enabled = false;
+            buttonRemove.Location = new Point(80, 268);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(73, 23);
+            buttonRemove.TabIndex = 6;
+            buttonRemove.Text = "Remove";
+            buttonRemove.UseVisualStyleBackColor = true;
+            buttonRemove.Click += buttonRemove_Click;
+            buttonRemove.KeyDown += Any_KeyDown;
+            // 
+            // buttonProcess
+            // 
+            buttonProcess.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonProcess.Location = new Point(12, 415);
+            buttonProcess.Name = "buttonProcess";
+            buttonProcess.Size = new Size(141, 23);
+            buttonProcess.TabIndex = 8;
+            buttonProcess.Text = "Process";
+            buttonProcess.UseVisualStyleBackColor = true;
+            buttonProcess.Click += buttonProcess_Click;
+            buttonProcess.KeyDown += Any_KeyDown;
+            // 
+            // labelCurrentFolder
+            // 
+            labelCurrentFolder.AutoEllipsis = true;
+            labelCurrentFolder.AutoSize = true;
+            labelCurrentFolder.Location = new Point(12, 38);
+            labelCurrentFolder.MaximumSize = new Size(141, 30);
+            labelCurrentFolder.Name = "labelCurrentFolder";
+            labelCurrentFolder.Size = new Size(86, 30);
+            labelCurrentFolder.TabIndex = 10;
+            labelCurrentFolder.Text = "Current Folder:\r\nNone";
+            // 
+            // buttonHelp
+            // 
+            buttonHelp.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonHelp.Location = new Point(12, 386);
+            buttonHelp.Name = "buttonHelp";
+            buttonHelp.Size = new Size(141, 23);
+            buttonHelp.TabIndex = 7;
+            buttonHelp.Text = "Help";
+            buttonHelp.UseVisualStyleBackColor = true;
+            buttonHelp.Click += buttonHelp_Click;
+            buttonHelp.KeyDown += Any_KeyDown;
+            // 
+            // galleryFolderDialog
+            // 
+            galleryFolderDialog.AddToRecent = false;
+            galleryFolderDialog.Description = "Select Gallery Location";
+            galleryFolderDialog.OkRequiresInteraction = true;
+            galleryFolderDialog.RootFolder = Environment.SpecialFolder.CommonPictures;
+            galleryFolderDialog.UseDescriptionForTitle = true;
+            // 
+            // textboxLog
+            // 
+            textboxLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            textboxLog.Location = new Point(844, 12);
+            textboxLog.Name = "textboxLog";
+            textboxLog.ReadOnly = true;
+            textboxLog.Size = new Size(239, 426);
+            textboxLog.TabIndex = 11;
+            textboxLog.Text = "Progress will be shown here!";
+            textboxLog.KeyDown += Any_KeyDown;
+            // 
+            // outputFolderDialog
+            // 
+            outputFolderDialog.AddToRecent = false;
+            outputFolderDialog.Description = "Select Output Folder";
+            outputFolderDialog.OkRequiresInteraction = true;
+            outputFolderDialog.RootFolder = Environment.SpecialFolder.MyPictures;
+            // 
+            // checkboxShouldMove
+            // 
+            checkboxShouldMove.AutoSize = true;
+            checkboxShouldMove.Location = new Point(12, 297);
+            checkboxShouldMove.Name = "checkboxShouldMove";
+            checkboxShouldMove.Size = new Size(82, 19);
+            checkboxShouldMove.TabIndex = 12;
+            checkboxShouldMove.Text = "Move Files";
+            checkboxShouldMove.UseVisualStyleBackColor = true;
+            checkboxShouldMove.CheckedChanged += checkboxShouldMove_CheckedChanged;
+            // 
+            // checkboxSpecialCats
+            // 
+            checkboxSpecialCats.AutoSize = true;
+            checkboxSpecialCats.Enabled = false;
+            checkboxSpecialCats.Location = new Point(12, 322);
+            checkboxSpecialCats.Name = "checkboxSpecialCats";
+            checkboxSpecialCats.Size = new Size(122, 19);
+            checkboxSpecialCats.TabIndex = 13;
+            checkboxSpecialCats.Text = "Special Categories";
+            checkboxSpecialCats.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1095, 450);
+            Controls.Add(checkboxSpecialCats);
+            Controls.Add(checkboxShouldMove);
+            Controls.Add(textboxLog);
+            Controls.Add(buttonHelp);
+            Controls.Add(labelCurrentFolder);
+            Controls.Add(buttonProcess);
+            Controls.Add(buttonRemove);
+            Controls.Add(buttonSubmit);
+            Controls.Add(labelShortcut);
+            Controls.Add(labelNewCategory);
+            Controls.Add(textboxShortcut);
+            Controls.Add(textboxCategory);
+            Controls.Add(listboxCategories);
+            Controls.Add(buttonOpenFolder);
+            Controls.Add(pictureBox);
+            Name = "MainForm";
+            Text = "GallerySwiper";
+            Click += ResetFocus;
+            KeyDown += Any_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+        private Button buttonOpenFolder;
+        private ListBox listboxCategories;
+        private TextBox textboxCategory;
+        private TextBox textboxShortcut;
+        private Label labelNewCategory;
+        private Label labelShortcut;
+        private Button buttonSubmit;
+        private Button buttonRemove;
+        private Button buttonProcess;
+        private Label labelCurrentFolder;
+        private Button buttonHelp;
+        private FolderBrowserDialog galleryFolderDialog;
+        private RichTextBox textboxLog;
+        private FolderBrowserDialog outputFolderDialog;
+        private CheckBox checkboxShouldMove;
+        private CheckBox checkboxSpecialCats;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+using System.Collections.ObjectModel;
+
+namespace GallerySwiper
+{
+    public partial class MainForm : Form
+    {
+        string helpText = @"Welcome to Gallery Swiper!
+
+In this app, you can press single keys to sort photos! Just add your categories with corresponding shortcuts and tap the keys while the images show up on the right!
+You can always go back with the reserved Shortcut Ctrl+Z and nothing gets moved before you click ""Process"" at the bottom!
+To get started open a folder and either use the defaults or edit/remove them!
+
+You cannot remove a category in the middle of the process (yet) if it contains items so be careful!
+
+The special categories checkbox enables actions to be performed automatically after!
+
+Thanks for using Gallery Swiper
+- Nora J.F.";
+
+        // Category Name, Shortcut
+        ObservableCollection<(string, Keys)> categories = [];
+        // From, To
+        List<(string, string)> sorted = [];
+        // index via sorted.Count
+        string[] filesToProcess = [];
+        IEnumerable<string> allowedFileExtensions = [
+            ".png",
+            ".jpg",
+            ".jpeg",
+            ".jfif",
+            ".gif",
+            ".webp",
+            ".avif"
+        ];
+
+        bool noWarning = true;
+
+        Keys currentShortcut = Keys.None;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            categories.CollectionChanged += UpdateListBox;
+            // Default
+            categories.Add(("Keep", Keys.Right));
+            categories.Add(("Delete", Keys.Left));
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(helpText, "Help", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+        }
+
+        private void Any_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Z && e.Control)
+            {
+                if (sorted.Count > 0)
+                {
+                    sorted.RemoveAt(sorted.Count - 1);
+                }
+            }
+            else if (filesToProcess.Length > 0)
+            {
+                string cat = categories.FirstOrDefault((a) => a.Item2 == (e.Modifiers | e.KeyCode)).Item1;
+                if (string.IsNullOrEmpty(cat)) return;
+                sorted.Add((filesToProcess[sorted.Count], cat));
+            }
+
+            LoadCurrentImage();
+            if (sorted.Count == filesToProcess.Length) MessageBox.Show("You're done! You can now press the \"Process\" Button or Undo with Ctrl+Z");
+            e.SuppressKeyPress = true;
+        }
+
+        private void UpdateListBox(object? sender, EventArgs e)
+        {
+            listboxCategories.Items.Clear();
+            // Needs to match indexes
+            for (int i = 0; i < categories.Count; i++)
+            {
+                var category = categories[i];
+                listboxCategories.Items.Add($"{category.Item1}\t{category.Item2.ToString()}");
+            }
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            if (textboxCategory.Text == "") return;
+            if (textboxShortcut.Text == "" || currentShortcut == Keys.None) return;
+
+            bool sel = listboxCategories.SelectedIndex != -1;
+
+            textboxCategory.Text = textboxCategory.Text.Trim();
+
+            if (Path.GetInvalidFileNameChars().Any(textboxCategory.Text.Contains) || textboxCategory.Text.ToLower() == "con")
+            {
+                MessageBox.Show("The Category name must be a valid directory name", "Invalid Category Name");
+                return;
+            }
+            if (categories.Any(a => !(sel && a.Equals(categories[listboxCategories.SelectedIndex])) && a.Item2 == currentShortcut)
+            )
+            {
+                MessageBox.Show("You cannot have the same shortcut for multiple categories!", "Duplicate Shortcut");
+                return;
+            }
+            if (categories.Any(a =>
+                !(sel && a.Equals(categories[listboxCategories.SelectedIndex]))
+                && a.Item1.Equals(textboxCategory.Text, StringComparison.CurrentCultureIgnoreCase))
+            )
+            {
+                MessageBox.Show("You cannot have the same category name for multiple categories!", "Duplicate Category Name");
+                return;
+            }
+
+            if (sel)
+            {
+                if (sorted.Count > 0 && sorted.Any(a => a.Item2 == categories[listboxCategories.SelectedIndex].Item1))
+                {
+                    // bad idea! :/
+                    sorted = sorted
+                        .Select(a => a.Item2 == categories[listboxCategories.SelectedIndex].Item1 ? (a.Item1, textboxCategory.Text) : a)
+                        .ToList();
+                }
+                categories[listboxCategories.SelectedIndex] = (textboxCategory.Text, currentShortcut);
+            }
+            else
+            {
+                categories.Add((textboxCategory.Text, currentShortcut));
+            }
+            textboxCategory.Text = "";
+            textboxShortcut.Text = "";
+            currentShortcut = Keys.None;
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listboxCategories.SelectedIndex == 1) return; // how
+            if (sorted.Count > 0 && sorted.Any(a => a.Item2 == categories[listboxCategories.SelectedIndex].Item1))
+            {
+                MessageBox.Show("You can't remove an already populated category!", "No!");
+                return;
+            }
+            listboxCategories.Items.RemoveAt(listboxCategories.SelectedIndex);
+        }
+
+        private void listboxCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listboxCategories.SelectedIndex != -1)
+            {
+                buttonSubmit.Text = "Submit";
+                buttonRemove.Enabled = true;
+
+                textboxCategory.Text = categories[listboxCategories.SelectedIndex].Item1;
+                currentShortcut = categories[listboxCategories.SelectedIndex].Item2;
+                textboxShortcut.Text = ToKeysString(currentShortcut);
+            }
+            else
+            {
+                buttonSubmit.Text = "Add";
+                buttonRemove.Enabled = false;
+            }
+        }
+
+        private void buttonOpenFolder_Click(object sender, EventArgs e)
+        {
+            if (galleryFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelCurrentFolder.Text = "Current Folder:\n" + galleryFolderDialog.SelectedPath;
+                this.Refresh();
+                filesToProcess =
+                    Directory
+                        .EnumerateFiles(galleryFolderDialog.SelectedPath, "", SearchOption.AllDirectories)
+                        .Where((a, _) => allowedFileExtensions.Any(f =>
+                            a.EndsWith(f)
+                        ))
+                        .ToArray();
+                if (filesToProcess.Length == 0)
+                {
+                    MessageBox.Show("No image files found under that path!", "Empty Result", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                LoadCurrentImage();
+            }
+        }
+
+        private void LoadCurrentImage()
+        {
+            try
+            {
+                if (sorted.Count < filesToProcess.Length)
+                    pictureBox.ImageLocation = filesToProcess[sorted.Count];
+                else pictureBox.ImageLocation = "";
+            }
+            catch (Exception ex)
+            {
+                if (MessageBox.Show(
+                    "Below is the Stack trace of this Exception. You can close the Application now (Click Yes) and you probably should if you don't know what this means.\n\n" + ex.StackTrace,
+                    ex.Message,
+                    MessageBoxButtons.YesNo) == DialogResult.Yes
+                )
+                {
+                    Application.Exit();
+                }
+            }
+            UpdateLog();
+        }
+
+        private void UpdateLog()
+        {
+            textboxLog.Text = $@"Progress: {sorted.Count}/{filesToProcess.Length} ({100.0d * sorted.Count / filesToProcess.Length}%)
+Last few actions:
+{(sorted.Count > 0
+    ? sorted
+        .TakeLast(10)
+        .Reverse()
+        .Select(a => $"{Path.GetRelativePath(galleryFolderDialog.SelectedPath, a.Item1)} sorted into {a.Item2}")
+        .Aggregate((a, b) => $"{a}\n{b}")
+    : "None!")}";
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            if (outputFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                ProcessFiles(outputFolderDialog.SelectedPath);
+            }
+        }
+
+        private void ProcessFiles(string root)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            textboxLog.Text = "Progress:\n-------- Initialize --------";
+            foreach (var category in categories)
+            {
+                Directory.CreateDirectory(Path.Join(root, category.Item1));
+                textboxLog.Text += $"\nCreated Directory {category.Item1}";
+                textboxLog.Refresh();
+            }
+            textboxLog.Text += checkboxShouldMove.Checked ? "\n-------- Moving --------" : "\n-------- Copying --------";
+            textboxLog.Refresh();
+            foreach (var sortTuple in sorted)
+            {
+                if (checkboxSpecialCats.Checked && sortTuple.Item2 == "Ignore") continue;
+                textboxLog.Text += $"\n{
+                    (checkboxShouldMove.Checked ? "Moving" : "Copying")
+                } {
+                    Path.GetRelativePath(galleryFolderDialog.SelectedPath, sortTuple.Item1)
+                } to {
+                    sortTuple.Item2
+                }...";
+                textboxLog.Refresh();
+            }
+            if (!checkboxSpecialCats.Checked)
+            {
+                MessageBox.Show("Done!", "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            textboxLog.Text += "\n-------- Special Actions --------";
+            if (categories.Any(a => a.Item1 == "Delete"))
+            {
+                textboxLog.Text = "\nDeleting the \"Delete\" Directory...";
+                Directory.Delete(Path.Join(root, "Delete"), true);
+            }
+        }
+
+        private void Shortcut_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey || e.KeyCode == Keys.ShiftKey || e.KeyCode == Keys.Menu) return;
+            if (e.KeyCode == Keys.Escape)
+            {
+                currentShortcut = Keys.None;
+                textboxShortcut.Text = "";
+            }
+            textboxShortcut.Text = ToKeysString(e.Modifiers | e.KeyCode);
+            currentShortcut = e.Modifiers | e.KeyCode;
+        }
+
+        private void ResetFocus(object sender, EventArgs e)
+        {
+            this.ActiveControl = textboxLog;
+            listboxCategories.SelectedIndex = -1;
+
+            textboxCategory.Text = "";
+            textboxShortcut.Text = "";
+            currentShortcut = Keys.None;
+        }
+
+        private string ToKeysString(Keys k)
+        {
+            Keys m = k & Keys.Modifiers;
+            bool ctrl = m.HasFlag(Keys.Control);
+            bool shift = m.HasFlag(Keys.Shift);
+            bool alt = m.HasFlag(Keys.Alt);
+            return (ctrl ? "Ctrl + " : "") + (shift ? "Shift + " : "") + (alt ? "Alt + " : "") + ((Keys)(k - m)).ToString();
+        }
+
+        private void checkboxShouldMove_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkboxShouldMove.Checked && noWarning)
+            {
+                if (MessageBox.Show("There is absolutely no warranty if data gets lost when moving instead of copying the files!", "Are you sure?", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.Cancel)
+                {
+                    checkboxShouldMove.Checked = false;
+                }
+                else noWarning = false;
+            }
+
+            if (checkboxShouldMove.Checked)
+                checkboxSpecialCats.Enabled = true;
+            else
+                checkboxSpecialCats.Enabled = false;
+        }
+    }
+}
